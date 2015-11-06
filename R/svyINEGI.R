3a695d54b331849ae4a85d0c0d7bb4f7959f64ea
jcms2665 <jcms2665@gmail.com>
2015-11-05 23:19:42 -0600

    Precisiones estadisticas

---
+++ R/svyINEGI.R
@@ -0,0 +1,27 @@
+#----- Primer intento para generar precisiones estadísticas -------
+
+
+svyINEGI<-function(u, data = parent.frame()){
+  if(!any(names(data) == u[1]))
+    stop("[!] La variable no está en la base")
+  
+  if(!is.numeric(data[,u]))
+    stop("[!] La variable debe ser numérica")
+  
+  data$UNO<-1
+  options(survey.lonely.psu="adjust")
+  mydesign <- svydesign(id = ~UPM,data = data,weight = ~FAC_MTI,strata = ~EST_D,nest=TRUE)
+  sy1<-round(svyby(~UNO,~data[,u], mydesign, svytotal), digits = 0)
+  #se1<-round(data.frame(SE(sy1)), digits = 0)
+  cv1<-round(data.frame(cv(sy1)*100), digits = 2)
+  #cf1<-round(data.frame(coef(sy1)), digits = 0)
+  li1<-round(data.frame(confint(sy1, level = 0.90)), digits = 0)
+  
+  cc <- matrix(NA,nrow=nrow(se1),ncol=1)
+  for(j in 1:nrow(se1)){
+    if(cv1[j,1]<15){cc[j,1]<-"Y"}else{cc[j,1]<-"N"}
+  }
+  r<-data.frame(sy1,cv1,li1,cc)
+  names(r) <- c("Categ","Estimacion","ErrStd","CV","LInf(90%)","LSup(90%)","¿Signif?")
+  return(r)
+}
